fee176f3aff2281e4aa0f1f0406aab81a39970c0
EmmyDev <emmyvera01@gmail.com>
2021-09-21 15:16:55 +0000

    Second Commit

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    ENV: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || 3000,
-    URL: process.env.BASE_URL || "http://localhost:3000",
-    MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/cotBackend",
-    user: process.env.MONGO_USER,
-    pass: process.env.MONGO_PASSWORD
+  ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || 'http://localhost:3000',
+  HOST: process.env.HOST || 'localhost',
+  DB_PORT: process.env.DB_PORT || 3306,
+  USER: process.env.USER || 'root',
+  PASSWORD: process.env.PASSWORD || '',
+  DATABASE: process.env.DATABASE || 'wedecide',
 }
--- database/confix.js
+++ database/confix.js
@@ -1,10 +1,12 @@
+const config = require('../config')
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'wedecide',
+  host: config.HOST,
+  port: config.DB_PORT,
+  user: 'admin',
+  password: config.PASSWORD,
+  database: config.DATABASE,
   multipleStatements: true,
 })
 
